e0432a276aeb9818cfeef49417ab2692bef95a26
nestcx <marcusvalentino2013@gmail.com>
2016-11-10 20:39:44 +1100

    colors, guess limiting, and cheating features added

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,6 +3,8 @@ require 'sinatra/reloader'
 
 NUMBER = rand(100)
 
+@@guesses_left = 5
+
 def check_guess(guess)
 	if guess.to_i > NUMBER
 		if guess.to_i - 5 > NUMBER
@@ -21,23 +23,35 @@ def check_guess(guess)
 	end
 end
 
-
 get '/' do
 	guess = params["guess"]
+	cheat = params["cheat"]
+
+	@@guesses_left = @@guesses_left - 1
+
+	if @@guesses_left == 0
+		NUMBER = rand(100)
+		@@guesses_left = 5
+	end
+
 	response = check_guess(guess)
+
 	if response == "correct"
 		message = NUMBER
-	else
+		color = "green"
+		NUMBER == rand(100)
+		@@guesses_left = 5
+	elsif response != "correct"
 		message = "?"
 	end
-		erb :index, :locals => {:message => message, :response => response}
+
+	message = NUMBER if cheat.to_s == "true"
+	
+	color = "tomato" if response == "way too high!"
+	color = "coral" if response == "too high!"
+	color = "gray" if response == "way too low!"
+	color = "silver" if response == "too low!"
+	erb :index, :locals => {:guesses_left => @@guesses_left, :color => color, :message => message, :response => response}
 end
-
-
-
-
-
-
-
 
 
